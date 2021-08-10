bfbf8c92482ba51105ca80a5b46fba9fd170763f
Jhoner Pineda <39860554+Zeroluffs@users.noreply.github.com>
2021-08-10 14:05:40 -0500

    it now filters by role (accepts any role) and makes sure both datasets have the same ammount of games played

--- api_app/plotting/plots.py
+++ api_app/plotting/plots.py
@@ -8,57 +8,77 @@ from matplotlib.pyplot import figure
 from api_app.plotting.functions import largeScaleGraphTest
 
 
-def damage_dealt(summoner_name, nGames, region):
+def damage_dealt(summoner_name, nGames, region, role):
 
     api_key = 'RGAPI-bed477c4-5dd4-48cd-8711-a6d10f9ce83f'
     watcher = LolWatcher(api_key)
     my_region = 'na1'
     me = watcher.summoner.by_name(region, summoner_name)
     my_id = me['puuid']
-
    # Return the rank status for me
-    my_ranked_stats = watcher.league.by_summoner(my_region, me['id'])
     my_matches = watcher.match_v5.matchlist_by_puuid(
         'AMERICAS', puuid=my_id, queue='420', type='ranked', count=nGames)
 
     participants = []
-    gameCount = 1
+    gameCount = 0
+
+    if role == "jg":
+        individualPosition = "JUNGLE"
+        teamPosition = "JUNGLE"
+    if role == "top":
+        individualPosition = "TOP"
+        teamPosition = "TOP"
+    if role == "mid":
+        individualPosition = "MIDDLE"
+        teamPosition = "MIDDLE"
+    if role == "adc":
+        individualPosition = "BOTTOM"
+        teamPosition = "BOTTOM"
+    if role == "supp":
+        individualPosition = "UTILITY"
+        teamPosition = "UTILITY"
+
     for n in range(nGames):
         match_detail = watcher.match_v5.by_id(
             region="AMERICAS", match_id=my_matches[n])
         filteredList = list(filter(lambda player: (
-            player['individualPosition'] == 'UTILITY' and player['teamPosition'] == 'UTILITY'), match_detail['info']['participants']))
+            player['individualPosition'] == individualPosition and player['teamPosition'] == teamPosition), match_detail['info']['participants']))
         k = len(filteredList)
-
         for i in range(k):
-            data_dict = filteredList[i]
-            data_dict['championName']
-            participants_row = {}
-            participants_row['gameCount'] = gameCount
-            participants_row['championName'] = data_dict["championName"]
-            participants_row['puuid'] = data_dict["puuid"]
-            participants_row['championId'] = data_dict["championId"]
-            participants_row['win'] = data_dict["win"]
-            participants_row['kills'] = data_dict["kills"]
-            participants_row['deaths'] = data_dict["deaths"]
-            participants_row['assists'] = data_dict["assists"]
-            participants_row['totalDamageDealtToChampions'] = data_dict["totalDamageDealtToChampions"]
-            participants_row["totalDamageTaken"] = data_dict["totalDamageTaken"]
-            participants_row['goldEarned'] = data_dict["goldEarned"]
-            participants_row['totalMinionsKilled'] = data_dict["totalMinionsKilled"]
-            participants_row["wardsPlaced"] = data_dict["wardsPlaced"]
-            participants_row["wardsKilled"] = data_dict["wardsKilled"]
-            participants_row["visionScore"] = data_dict["visionScore"]
-            participants_row["visionWardsBoughtInGame"] = data_dict["visionWardsBoughtInGame"]
-            participants_row["totalHealsOnTeammates"] = data_dict["totalHealsOnTeammates"]
-            participants_row["totalTimeSpentDead"] = data_dict["totalTimeSpentDead"]
-            participants_row["totalDamageShieldedOnTeammates"] = data_dict["totalDamageShieldedOnTeammates"]
-            participants.append(participants_row)
+            if k == 2:
+                data_dict = filteredList[i]
+                data_dict['championName']
+                participants_row = {}
+                participants_row['gameCount'] = gameCount
+                participants_row['championName'] = data_dict["championName"]
+                participants_row['puuid'] = data_dict["puuid"]
+                participants_row['championId'] = data_dict["championId"]
+                participants_row['win'] = data_dict["win"]
+                participants_row['kills'] = data_dict["kills"]
+                participants_row['deaths'] = data_dict["deaths"]
+                participants_row['assists'] = data_dict["assists"]
+                participants_row['totalDamageDealtToChampions'] = data_dict["totalDamageDealtToChampions"]
+                participants_row["totalDamageTaken"] = data_dict["totalDamageTaken"]
+                participants_row['goldEarned'] = data_dict["goldEarned"]
+                participants_row['totalMinionsKilled'] = data_dict["totalMinionsKilled"]
+                participants_row["wardsPlaced"] = data_dict["wardsPlaced"]
+                participants_row["wardsKilled"] = data_dict["wardsKilled"]
+                participants_row["visionScore"] = data_dict["visionScore"]
+                participants_row["visionWardsBoughtInGame"] = data_dict["visionWardsBoughtInGame"]
+                participants_row["totalHealsOnTeammates"] = data_dict["totalHealsOnTeammates"]
+                participants_row["totalTimeSpentDead"] = data_dict["totalTimeSpentDead"]
+                participants_row["totalDamageShieldedOnTeammates"] = data_dict["totalDamageShieldedOnTeammates"]
+                participants.append(participants_row)
         gameCount = gameCount + 1
+        
 
     df = pd.DataFrame(participants)
     personal_data = df.loc[(df['puuid'] == my_id)]
+    print(personal_data['totalDamageDealtToChampions'])
+
     enemy_data = df.loc[(df['puuid'] != my_id)]
+    print(len(enemy_data['totalDamageDealtToChampions']))
+
     damage_data = damageDone(
         personal_data, enemy_data, summoner_name)
     gold_data = goldEarned(
@@ -75,16 +95,6 @@ def damage_dealt(summoner_name, nGames, region):
 
 
 def damageDone(my_jungle_games, enemy_jungle_games, summoner_name):
-    enemy_size = enemy_jungle_games.shape[0]
-    my_size = my_jungle_games.shape[0]
-    arrayIndex = []
-    if enemy_size > my_size:
-        for row in range(enemy_size, my_size, -1):
-            print("sup", row)
-            arrayIndex.append(row-1)
-    enemy_jungle_games = enemy_jungle_games.drop(
-        enemy_jungle_games.index[arrayIndex])
-    print(my_jungle_games)
     ax = my_jungle_games.plot(kind="line", x='gameCount',
                               y='totalDamageDealtToChampions', marker='o', label=summoner_name)
     enemy_jungle_games.plot(ax=ax, kind='line', x='gameCount',
@@ -108,15 +118,6 @@ def damageDone(my_jungle_games, enemy_jungle_games, summoner_name):
 
 
 def goldEarned(my_jungle_games, enemy_jungle_games, summoner_name):
-    enemy_size = enemy_jungle_games.shape[0]
-    my_size = my_jungle_games.shape[0]
-    arrayIndex = []
-    if enemy_size > my_size:
-        for row in range(enemy_size, my_size, -1):
-            print("sup", row)
-            arrayIndex.append(row-1)
-    enemy_jungle_games = enemy_jungle_games.drop(
-        enemy_jungle_games.index[arrayIndex])
     ax = my_jungle_games.plot(kind="line", x='gameCount',
                               y='goldEarned', marker='o', label=summoner_name)
     enemy_jungle_games.plot(ax=ax, kind='line', x='gameCount',
